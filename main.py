d42275fff1490ed4e113c4be17e78c08bd7dc466
Sayama0310 <montblanc3060@gmail.com>
2023-01-12 08:45:13 +0900

    Hueの画像を生成するコードを作成

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from PIL import Image
 import colorsys
 
-# グレースケールの画像データを作成
 img = Image.new("RGB", (256, 256), (255, 255, 255))
 size = img.size
 
@@ -10,6 +9,14 @@ for x in range(size[0]):
         (r, g, b) = colorsys.hsv_to_rgb(212 / 360, x / 255, (255 - y) / 255)
         img.putpixel((x, y), (int(r * 256), int(g * 256), int(b * 256)))
 
-# 画像の表示
 img.save('./result/color.png')
 
+img = Image.new("RGB", (256, 20), (255, 255, 255))
+size = img.size
+
+for x in range(size[0]):
+    for y in range(size[1]):
+        (r, g, b) = colorsys.hsv_to_rgb(x / 255, 1, 1)
+        img.putpixel((x, y), (int(r * 256), int(g * 256), int(b * 256)))
+
+img.save('./result/hue.png')
