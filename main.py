0f9dc9d3c78531f237eb14bff7afd6e9b30bc255
Sayama0310 <montblanc3060@gmail.com>
2023-01-12 08:39:31 +0900

    HSV色空間を生成する画像

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Hello')
+from PIL import Image
+import colorsys
+
+# グレースケールの画像データを作成
+img = Image.new("RGB", (256, 256), (255, 255, 255))
+size = img.size
+
+for x in range(size[0]):
+    for y in range(size[1]):
+        (r, g, b) = colorsys.hsv_to_rgb(212 / 360, x / 255, (255 - y) / 255)
+        img.putpixel((x, y), (int(r * 256), int(g * 256), int(b * 256)))
+
+# 画像の表示
+img.save('./result/color.png')
+
